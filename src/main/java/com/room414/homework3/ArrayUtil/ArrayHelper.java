df5fb665d48ef61d4d9fc37009f7c669fb54532b
melalex <melalex490@gmail.com>
2017-02-21 17:14:01 +0200

    ArrayHelper init

---
+++ src/main/java/com/room414/homework3/ArrayUtil/ArrayHelper.java
@@ -0,0 +1,25 @@
+package com.room414.homework3.ArrayUtil;
+
+import java.util.Arrays;
+
+/**
+ * @author Alexander Melashchenko
+ * @version 1.0 21 Feb 2017
+ */
+public class ArrayHelper {
+    private ArrayHelper() {
+
+    }
+
+    public static <T> T[] quickSort(T[] array) {
+        T[] result = array.clone();
+
+
+
+        return result;
+    }
+
+    private static <T> void doSort(T[] array) {
+
+    }
+}
---
+++ src/test/java/com/room414/homework3/ArrayUtil/ArrayHelperTest.java
@@ -0,0 +1,17 @@
+package com.room414.homework3.ArrayUtil;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Alexander Melashchenko
+ * @version 1.0 21 Feb 2017
+ */
+class ArrayHelperTest {
+    @Test
+    void quickSort() {
+
+    }
+
+}
